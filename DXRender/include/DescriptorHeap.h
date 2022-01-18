b8e13efdbb465c39f846278148c94622fee52458
ChenWang <1401520906@qq.com>
2022-01-18 19:57:10 +0800

    wrap descriptor heap

---
+++ DXRender/include/DescriptorHeap.h
@@ -0,0 +1,47 @@
+#ifndef __DESCRIPTORHEAP_H__
+#define __DESCRIPTORHEAP_H__
+
+#include <d3d12.h>
+#include <wrl.h>
+
+using Microsoft::WRL::ComPtr;
+
+class DescriptorHeap
+{
+protected:
+    ComPtr<ID3D12DescriptorHeap> m_heap;
+    UINT m_numDescriptors;
+    DescriptorHeap(ComPtr<ID3D12Device2> device, UINT num, D3D12_DESCRIPTOR_HEAP_TYPE type,
+        D3D12_DESCRIPTOR_HEAP_FLAGS flag);
+public:
+    // virtual D3D12_DESCRIPTOR_HEAP_TYPE GetType() const = 0;
+    // virtual D3D12_CPU_DESCRIPTOR_HANDLE GetDescriptorHandle() const = 0;
+    ComPtr<ID3D12DescriptorHeap> GetHeap() const;
+};
+
+class RTVDescriptorHeap : public DescriptorHeap
+{
+public:
+    static const D3D12_DESCRIPTOR_HEAP_TYPE S_TYPE = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+private:
+    UINT m_RTVDescriptorSize;
+
+public:
+    RTVDescriptorHeap(ComPtr<ID3D12Device2> device, UINT num, D3D12_DESCRIPTOR_HEAP_FLAGS flag = D3D12_DESCRIPTOR_HEAP_FLAG_NONE);
+    D3D12_DESCRIPTOR_HEAP_TYPE GetType() const;
+    D3D12_CPU_DESCRIPTOR_HANDLE GetDescriptorHandle(UINT currentBackBufferIndex) const;
+};
+
+class DSVDescriptorHeap : public DescriptorHeap
+{
+public:
+    static const D3D12_DESCRIPTOR_HEAP_TYPE S_TYPE = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+
+public:
+    DSVDescriptorHeap(ComPtr<ID3D12Device2> device, UINT num, D3D12_DESCRIPTOR_HEAP_FLAGS flag = D3D12_DESCRIPTOR_HEAP_FLAG_NONE);
+
+    D3D12_DESCRIPTOR_HEAP_TYPE GetType() const;
+    D3D12_CPU_DESCRIPTOR_HANDLE GetDescriptorHandle() const;
+};
+
+#endif
---
+++ DXRender/src/DescriptorHeap.cpp
@@ -0,0 +1,51 @@
+#include "DescriptorHeap.h"
+#include "helper.h"
+
+DescriptorHeap::DescriptorHeap(ComPtr<ID3D12Device2> device, UINT num, D3D12_DESCRIPTOR_HEAP_TYPE type,
+        D3D12_DESCRIPTOR_HEAP_FLAGS flag)
+    : m_numDescriptors(num)
+{
+    D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+    desc.NumDescriptors = num;
+    desc.Type = type;
+    desc.Flags = flag;
+    ThrowIfFailed(device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&m_heap)));
+}
+
+RTVDescriptorHeap::RTVDescriptorHeap(ComPtr<ID3D12Device2> device, UINT num,
+        D3D12_DESCRIPTOR_HEAP_FLAGS flag)
+    : DescriptorHeap(device, num, S_TYPE, flag)
+{
+    m_RTVDescriptorSize = device->GetDescriptorHandleIncrementSize(S_TYPE);
+}
+
+DSVDescriptorHeap::DSVDescriptorHeap(ComPtr<ID3D12Device2> device, UINT num,
+        D3D12_DESCRIPTOR_HEAP_FLAGS flag)
+    : DescriptorHeap(device, num, S_TYPE, flag)
+{
+}
+
+ComPtr<ID3D12DescriptorHeap> DescriptorHeap::GetHeap() const
+{
+    return m_heap;
+}
+
+D3D12_DESCRIPTOR_HEAP_TYPE RTVDescriptorHeap::GetType() const
+{
+    return S_TYPE;
+}
+D3D12_DESCRIPTOR_HEAP_TYPE DSVDescriptorHeap::GetType() const
+{
+    return S_TYPE;
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE RTVDescriptorHeap::GetDescriptorHandle(UINT currentBackBufferIndex) const
+{
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtv(m_heap->GetCPUDescriptorHandleForHeapStart(), 
+        currentBackBufferIndex, m_RTVDescriptorSize);
+    return rtv;
+}
+D3D12_CPU_DESCRIPTOR_HANDLE DSVDescriptorHeap::GetDescriptorHandle() const
+{
+    return m_heap->GetCPUDescriptorHandleForHeapStart();
+}
